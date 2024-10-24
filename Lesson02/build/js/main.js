1438253565c763b01e808bfeb4bad5fcef541d39
Tomasz Wiśniewski <thomschery@gmail.com>
2024-10-24 11:17:31 +0200

    Lesson02

---
+++ Lesson02/build/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+let myName;
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'Dave';
+meaningOfLife = 42;
+isLoading = true;
+album = 'Van Halen';
+const sum = (a, b) => {
+    return a + b;
+};
---
+++ Lesson02/src/main.ts
@@ -0,0 +1,13 @@
+let myName: string;
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: string | number
+
+myName = 'Dave'
+meaningOfLife =42
+isLoading =true
+album = 'Van Halen'
+
+const sum = (a:number, b:string) =>{
+    return a + b
+}
